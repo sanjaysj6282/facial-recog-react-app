bf66f6ae1d4e59c5eb96d15e17b4270be3671103
Sanjay <sanjaysj6282@gmail.com>
2022-05-21 15:50:33 +0530

    added 1 more card

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -24,6 +24,7 @@ function Mainpage() {
               </div>
             </div>
           </div>
+          
           <div className="col-md-4">
             <div className="card" style={{ width: "18rem" }}>
               <img
@@ -35,6 +36,24 @@ function Mainpage() {
                 <h5 className="card-title">Add attendance</h5>
                 <p className="card-text">
                   To register attendance.
+                </p>
+                <a href="/" className="btn btn-primary">
+                  Click here
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card" style={{ width: "18rem" }}>
+              <img
+                src="https://images.ctfassets.net/lzny33ho1g45/60IgGQVSrxrUf5IAYdpxnc/e11f2cd9185c28bbc82462a0602b6e83/google-drive-vs-google-photos-00-hero.png?w=1520&fm=jpg&q=30&fit=thumb&h=760"
+                className="card-img-top"
+                alt="..."
+              />
+              <div className="card-body">
+                <h5 className="card-title">View Attendance</h5>
+                <p className="card-text">
+                  To View attendance.
                 </p>
                 <a href="/" className="btn btn-primary">
                   Click here
