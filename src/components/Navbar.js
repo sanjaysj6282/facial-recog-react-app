3003a1871640c61d5e03c26bcb9914f880ecdefe
Sanjay <sanjaysj6282@gmail.com>
2022-05-21 12:42:31 +0530

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
     <div>
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
+                <a class="navbar-brand" href="#">Smart Attendance System </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
@@ -15,13 +15,7 @@ function Navbar() {
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
                     </li>
                     <li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link disabled">About</a>
                     </li>
                 </ul>
                 </div>
