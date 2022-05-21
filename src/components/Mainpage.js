113b53a56ae68b99ec3da8292bc3ae4aa3b6bb4b
Sanjay <sanjaysj6282@gmail.com>
2022-05-21 15:57:02 +0530

    added images

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -9,7 +9,7 @@ function Mainpage() {
           <div className="col-md-4">
             <div className="card" style={{ width: "18rem" }}>
               <img
-                src="https://images.ctfassets.net/lzny33ho1g45/60IgGQVSrxrUf5IAYdpxnc/e11f2cd9185c28bbc82462a0602b6e83/google-drive-vs-google-photos-00-hero.png?w=1520&fm=jpg&q=30&fit=thumb&h=760"
+                src="../images/add_attendance.png"
                 className="card-img-top"
                 alt="..."
               />
