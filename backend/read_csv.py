5d28c31847cfcb1ee623a05afab5e6d113df1e96
Sanjay <sanjaysj6282@gmail.com>
2022-05-26 14:29:41 +0530

    atte

---
+++ backend/read_csv.py
@@ -0,0 +1,20 @@
+import pandas
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    filename = 'static/logcsv.csv'
+
+    # to read the csv file using the pandas library
+    data = pandas.read_csv(filename, header=0)
+
+    myData = data.values
+    print(myData)
+    return render_template('home.html', myData=myData)
+
+
+if __name__ == '__main__':
+    app.run()
